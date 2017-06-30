351f31bbc8539a97e0c01f1aa4f283816426b702
Łukasz Harasimowicz <dev@harnash.eu>
2017-06-30 14:12:22 +0200

    Added more test cases to time_series package

--- model/time_series_test.go
+++ model/time_series_test.go
@@ -11,18 +11,18 @@ import (
 
 var _ = Describe("TimeSeries", func() {
 	ts := NewTimeSeries(10 * time.Second)
+	// we should normalize time to be able to predict the buckets they will fall into
+	startTime := time.Now().Round(10 * time.Second).Add(time.Second)
+
 	BeforeEach(func() {
 		ts.Clear()
+		Expect(ts.GetBucketCount()).To(Equal(0), "Check if there are no buckets in TimeSeries")
 	})
 
-	It("Should be able to handle multiple TimePointUInt64", func() {
-		startTime := time.Now()
+	It("Should be able to handle single TimePointUInt64", func() {
 		pt := NewTimePointUInt64(startTime, 1)
 
-		Expect(ts.GetBucketCount()).To(Equal(0), "Check if there are no buckets in TimeSeries")
-
 		err := ts.Append(&pt)
-
 		Expect(err).NotTo(HaveOccurred(), "Successfully added TimePoint")
 		Expect(ts.GetBucketCount()).To(Equal(1), "Bucket count should increase by 1")
 
@@ -33,5 +33,33 @@ var _ = Describe("TimeSeries", func() {
 				Expect(item).To(BeEquivalentTo(&pt))
 			}
 		}
+	})
+
+	It("Should properly separate TimePoints into buckets", func() {
+		pt1 := NewTimePointUInt64(startTime, 1)
+		pt2 := NewTimePointUInt64(startTime.Add(5*time.Second), 2)
+		pt3 := NewTimePointUInt64(startTime.Add(10*time.Second), 3)
+		pt4 := NewTimePointUInt64(startTime.Add(15*time.Second), 4)
+
+		err := ts.Append(&pt1)
+		Expect(err).NotTo(HaveOccurred(), "Successfully added #1 TimePoint")
+		err = ts.Append(&pt2)
+		Expect(err).NotTo(HaveOccurred(), "Successfully added #2 TimePoint")
+
+		Expect(ts.GetBucketCount()).To(Equal(1), "There should be exactly 1 bucket")
+
+		err = ts.Append(&pt3)
+		Expect(err).NotTo(HaveOccurred(), "Successfully added #3 TimePoint")
+		err = ts.Append(&pt4)
+		Expect(err).NotTo(HaveOccurred(), "Successfully added #4 TimePoint")
+
+		Expect(ts.GetBucketCount()).To(Equal(2), "There should be exactly 2 buckets")
+
+		pt5 := NewTimePointUInt64(startTime.Add(21*time.Second), 5)
+		err = ts.Append(&pt5)
+		Expect(err).NotTo(HaveOccurred(), "Successfully added #5 TimePoint")
+
+		Expect(ts.GetBucketCount()).To(Equal(3), "There should be exactly 3 buckets")
+
 	})
 })
