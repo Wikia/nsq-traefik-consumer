6e25a6a6ee851b81846ac47482eec08928e6cd5d
Łukasz Harasimowicz <dev@harnash.eu>
2017-10-16 15:02:26 +0200

    Fixing case when requests has no frontend specified

--- metrics/definitions.go
+++ metrics/definitions.go
@@ -2,4 +2,4 @@ package metrics
 
 import "regexp"
 
-var ApacheCombinedLogRegex = regexp.MustCompile(`^(?P<client_host>\S+)\s-\s+(?P<client_username>\S+\s+)+\[(?P<timestamp>[^]]+)\]\s"(?P<request_method>\S*)\s?(?P<request_path>(?:[^"]*(?:\\")?)*)\s(?P<request_protocol>[^"]*)"\s(?P<origin_status>\d+)\s(?P<origin_content_size>\d+)\s"(?P<request__referer>(?:[^"]*(?:\\")?)*)"\s"(?P<request__useragent>.*)"\s(?P<request_count>\d+)\s"(?P<frontend_name>[^"]+)"\s"(?P<backend_url>[^"]+)"\s(?P<duration>\d+)ms$`)
+var ApacheCombinedLogRegex = regexp.MustCompile(`^(?P<client_host>\S+)\s-\s+(?P<client_username>\S+\s+)+\[(?P<timestamp>[^]]+)\]\s"(?P<request_method>\S*)\s?(?P<request_path>(?:[^"]*(?:\\")?)*)\s(?P<request_protocol>[^"]*)"\s(?P<origin_status>\d+)\s(?P<origin_content_size>\d+)\s"(?P<request__referer>(?:[^"]*(?:\\")?)*)"\s"(?P<request__useragent>.*)"\s(?P<request_count>\d+)\s"(?P<frontend_name>[^"]*)"\s"(?P<backend_url>[^"]*)"\s(?P<duration>\d+)ms$`)
